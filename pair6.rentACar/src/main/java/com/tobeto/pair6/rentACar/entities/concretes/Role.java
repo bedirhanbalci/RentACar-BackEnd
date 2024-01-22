306b2f69897c8278d9bd9c50e86fe61ed6782fbf
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-01-22 16:48:55 +0300

    Added Security Configuration and Password Encoder Configuration

--- pair6.rentACar/src/main/java/com/tobeto/pair6/rentACar/entities/concretes/Role.java
+++ pair6.rentACar/src/main/java/com/tobeto/pair6/rentACar/entities/concretes/Role.java
@@ -1,21 +1,13 @@
 package com.tobeto.pair6.rentACar.entities.concretes;
 
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
-@Getter
 public enum Role implements GrantedAuthority {
 
-    ROLE_USER("USER"),
-    ROLE_ADMIN("ADMIN"),
-    ROLE_MODERATOR("MODERATOR"),
-    ROLE_MANAGER("MANAGER");
-
-    private final String value;
-
-    Role(String value) {
-        this.value = value;
-    }
+    USER,
+    ADMIN,
+    MODERATOR,
+    MANAGER;
 
     @Override
     public String getAuthority() {
